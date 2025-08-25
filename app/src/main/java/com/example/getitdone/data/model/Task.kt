0f4b8b8e979a3e3e9951686198f67587a693597f
Alan Amalemba <alan.amalemba@lctafrica.net>
2025-08-25 09:12:53 +0300

    add foreign key

--- app/src/main/java/com/example/getitdone/data/model/Task.kt
+++ app/src/main/java/com/example/getitdone/data/model/Task.kt
@@ -2,13 +2,23 @@ package com.example.getitdone.data.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity("tasks")
+@Entity(
+    tableName = "tasks",
+    foreignKeys = [ForeignKey(
+        entity = TasksList::class,
+        parentColumns = ["id"],
+        childColumns = ["list_id"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 data class Task(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
     val description: String? = null,
     @ColumnInfo("is_starred") val isStarred: Boolean = false,
-    @ColumnInfo("is_complete") val isComplete: Boolean = false
+    @ColumnInfo("is_complete") val isComplete: Boolean = false,
+    @ColumnInfo(name = "list_id") val listId: Int
 )
